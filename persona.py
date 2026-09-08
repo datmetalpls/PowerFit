3d5738b46cdc2148c2a7120b4a6cd8f19896970c
joseluisarriagadac-droid <jose.luis.arriagadac@gmail.com>
2026-09-07 21:29:53 -0300

    feat: agregar clase Persona vacia con comentarios

---
+++ persona.py
@@ -0,0 +1,4 @@
+# Define la clase llamada Persona que servirá como plantilla para representar a una persona en el sistema.
+class Persona:
+    # La instrucción 'pass' se utiliza como un marcador de posición que permite definir una clase vacía sin atributos ni métodos por ahora.
+    pass
