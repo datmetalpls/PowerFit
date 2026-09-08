d5203133c1aee28d9aa77092e2bc79cc9c24852a
datmetalpls <cguerreroa03@gmail.com>
2026-09-07 21:40:13 -0300

    Se hace solo un cambio para quitarte el miedo

--- persona.py
+++ persona.py
@@ -1,4 +1,10 @@
 # Define la clase llamada Persona que servirá como plantilla para representar a una persona en el sistema.
 class Persona:
     # La instrucción 'pass' se utiliza como un marcador de posición que permite definir una clase vacía sin atributos ni métodos por ahora.
+    rut: str
+    nombres: str
+    apellidoPaterno: str
+    apellidoMaterno: str
+    telefono: str
+    correoElectronico: str
     pass
