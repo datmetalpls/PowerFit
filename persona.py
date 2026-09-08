53b03f78a77ca87d2dd409d8026abcf4369ba16f
datmetalpls <cguerreroa03@gmail.com>
2026-09-07 21:53:13 -0300

    Actualizar declaracion de atributos en Persona segun diagrama UML

--- persona.py
+++ persona.py
@@ -1,12 +1,7 @@
-# Define la clase llamada Persona que servirá como plantilla para representar a una persona en el sistema.
 class Persona:
-    # Método constructor que inicializa los atributos de una nueva instancia de la clase Persona.
-    def __init__(self, rut: str, nombre: str, apellidoPaterno: str, apellidoMaterno: str):
-        # Asigna el parámetro 'rut' de tipo str al atributo de instancia correspondiente.
-        self.rut = rut
-        # Asigna el parámetro 'nombre' de tipo str al atributo de instancia correspondiente.
-        self.nombre = nombre
-        # Asigna el parámetro 'apellidoPaterno' de tipo str al atributo de instancia correspondiente.
-        self.apellidoPaterno = apellidoPaterno
-        # Asigna el parámetro 'apellidoMaterno' de tipo str al atributo de instancia correspondiente.
-        self.apellidoMaterno = apellidoMaterno
+    rut: str
+    nombre: str
+    apellidoPaterno: str
+    apellidoMaterno: str
+    telefono: str
+    correoElectronico: str
